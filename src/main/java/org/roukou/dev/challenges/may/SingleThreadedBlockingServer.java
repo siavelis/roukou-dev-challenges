4c8db651ac47c6be5733be61c287b8e1eb77d235
Thomas Pliakas <tpliakas@cognity.gr>
2021-05-19 19:45:41 +0300

    My Solutions

---
+++ src/main/java/org/roukou/dev/challenges/may/SingleThreadedBlockingServer.java
@@ -0,0 +1,39 @@
+package org.roukou.dev.challenges.may;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SingleThreadedBlockingServer {
+
+  private static final int SERVER_PORT = 8080;
+
+  public static void main(String[] args) throws IOException {
+
+    ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+
+    while (true) {
+      Socket socket = serverSocket.accept();
+
+      InputStream inputStream = socket.getInputStream();
+      OutputStream outputStream = socket.getOutputStream();
+
+      int inputData;
+
+      while ((inputData = inputStream.read()) != -1) {
+        outputStream.write(responseHandler(inputData));
+      }
+    }
+  }
+
+  private static int responseHandler(int inputData) {
+
+    if (!Character.isLetter(inputData)) {
+      return inputData;
+    } else {
+      return inputData ^ ' ';
+    }
+  }
+}
